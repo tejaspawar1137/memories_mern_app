c19de1d6926deacbe09af8271d53b1c8b647de79
tejas pawar <tejaspawar183@gmail.com>
2022-03-29 13:22:07 +0530

    memoriesapp

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
-
+dotenv.config()
 const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://tejaspawar:tejaspawar@cluster0.e1tuq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.REACT_APP_MONGODB_URL
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
